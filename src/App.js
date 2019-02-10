d8201d180fe6797e373d7a1326bdf56c75c2fff2
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-02-10 10:14:35 -0500

    updated styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-// import logo from "./logo.svg";
 import FriendCard from "./components/FriendCard";
 import Nav from "./components/Nav";
 import Wrapper from "./components/Wrapper";
